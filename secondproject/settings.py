4ff70082588119236025956c3aa6d9421a7204e0
chan mi <ccm3326@gmail.com>
2019-07-29 20:43:23 +0900

    first commit

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '^*k72a&(1tq^2gr!j#uhv2+jszl!&n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
